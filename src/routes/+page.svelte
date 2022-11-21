a3f35d65a5429bcc5277a636c4ae7d044fd17f2f
Ismail Sacic <cicas03@hotmail.com>
2022-11-21 21:11:32 +0100

    Homepage done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  	import { Card } from "flowbite-svelte";
+	import type { PageData } from './$types';
+	export let data: PageData;
+	
+</script>
+
+<div class="flex flex-col gap-4 sm:flex-row ">
+	{#each data.items as nav_item}
+		<Card href="/{nav_item.slug}" class="flex items-center w-full mx-auto py-8"  >
+				<img  alt="{nav_item.title}" src={nav_item.icon}  class="w-16 sm:w-12"/>
+				<h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{nav_item.title}</h5>
+				<p class="mb-3 font-normal text-gray-500 text-center dark:text-gray-400">{nav_item.descrtiption}</p>
+		</Card>
+	{/each}
+</div>
